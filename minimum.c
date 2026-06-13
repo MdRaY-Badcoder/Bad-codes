0dfc76abd75dd2c6642093bcf1563a524e73fe2d
MdRaY-Badcoder <md.a.a.rayhan@gmail.com>
2026-06-14 01:04:29 +0600

    Add files via upload

---
+++ minimum.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+
+int minimum(int a,int b){
+
+if(a<b){
+    return a;
+}
+else {
+    return b;
+}
+}
+
+int main(){
+
+int a,b;
+
+scanf("%d %d",&a,&b);
+
+printf("minimum = %d",minimum(a,b));
+
+return 0;
+}
