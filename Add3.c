17a7603534a1de1d2bfdf68db66493a6c13e3978
MdRaY-Badcoder <md.a.a.rayhan@gmail.com>
2026-06-14 18:06:22 +0600

    Add files via upload

---
+++ Add3.c
@@ -0,0 +1,14 @@
+#include <math.h>
+
+int add3(int a, int b,int c){
+return a+b+c;
+
+}
+int main(){
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+
+printf("sum = %d",add3(a,b,c));
+
+return 0;
+}
