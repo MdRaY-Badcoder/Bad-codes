475be39e6ea461ecc934b590ac679445d5af36b4
MdRaY-Badcoder <md.a.a.rayhan@gmail.com>
2026-06-29 22:35:06 +0600

    Add files via upload

---
+++ Prime number from 1 to N.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+
+int n;
+scanf("%d",&n);
+for(int i=2;i<=n;i++)
+{
+int prime=1;
+for(int j=2;j*j<=i;j++){
+    if(i%j==0){
+       prime=0;
+        break;
+    }
+}
+if(prime){
+    printf("%d ",i);
+}
+}
+
+return 0;
+}
