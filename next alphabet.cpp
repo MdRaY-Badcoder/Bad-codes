5d34db79306a3e1770bda300908478dbcecf8f0f
MdRaY-Badcoder <md.a.a.rayhan@gmail.com>
2026-07-11 17:36:54 +0600

    Create next alphabet.cpp

---
+++ next alphabet.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+ 
+     char a;
+      cin>>a;
+ 
+      if(a=='z'){
+        cout<<'a';
+      }
+      else{
+      cout<<char(a+1);
+      }
+    return 0;
+}
