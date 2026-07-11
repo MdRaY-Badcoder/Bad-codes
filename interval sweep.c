833b3ea116e1bee2a54f6199c0b101e8df0fb69a
MdRaY-Badcoder <md.a.a.rayhan@gmail.com>
2026-07-11 17:30:29 +0600

    Create interval sweep.c

---
+++ interval sweep.c
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+ 
+       int a,b;
+       cin>>a>>b;
+ 
+       if(abs(a - b) <= 1 && a + b >= 1){
+ 
+             cout<<"YES";
+            }
+       else{
+          cout<<"NO";
+       }
+ 
+ 
+ 
+    return 0;
+}
