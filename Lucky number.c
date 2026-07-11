128e237631f382ff1a827fbed0e05640d4c0e732
MdRaY-Badcoder <md.a.a.rayhan@gmail.com>
2026-07-11 17:29:50 +0600

    Create Lucky number.c

---
+++ Lucky number.c
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+ 
+    int N;
+    cin >> N;
+ 
+ 
+ 
+    int a=N / 10;
+    int b=N % 10;
+    if ((b != 0 && a % b == 0) || (a != 0 && b % a == 0)) {
+        cout <<"YES";
+    } else {
+        cout <<"NO";
+    }
+  
+    return 0;
+}
