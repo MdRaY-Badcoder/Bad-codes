0fcc1e0ea81e36856401eeb0d5c38c4ad99dad82
MdRaY-Badcoder <md.a.a.rayhan@gmail.com>
2026-07-24 21:53:56 +0600

    Create 158A- Next round.c

---
+++ 158A- Next round.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+int n,k;
+scanf("%d %d",&n,&k);
+int array[n];
+
+for(int i=0;i<n;i++){
+    scanf("%d",&array[i]);
+
+}
+int count=0;
+int kth=array[k-1];
+for(int i=0;i<n;i++){
+if(array[i]>=kth && array[i]>0){
+    count++;
+}
+}
+printf("%d\n",count);
+
+    return 0;
+}
