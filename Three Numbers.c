673b8e262daeef4be30ce323901c739c87886863
MdRaY-Badcoder <md.a.a.rayhan@gmail.com>
2026-07-28 21:34:08 +0600

    Create Three Numbers.c

---
+++ Three Numbers.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+
+int main(){
+int K,S;
+scanf("%d %d",&K,&S);
+int count=0;
+
+for(int x=0;x<=K;x++){
+    for(int y=0;y<=K;y++){
+        int z=S-x-y;
+    if(z>=0&&z<=K){
+        count++;
+    }
+  }
+}
+printf("%d\n",count);
+return 0;
+}
