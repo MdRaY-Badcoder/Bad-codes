59779dbe20f3213900c9be7b92bf36dbaaa801c8
MdRaY-Badcoder <md.a.a.rayhan@gmail.com>
2026-08-03 23:16:21 +0600

    Create Mirror Array.c

---
+++ Mirror Array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+int main(){
+int n,m;
+scanf("%d %d",&n,&m);
+
+int array[n][m];
+
+for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+        scanf("%d",&array[i][j]);
+        }
+}
+for(int i=0;i<n;i++){
+    for(int j=m-1;j>=0;j--){
+        printf("%d ",array[i][j]);
+    }
+    printf("\n");
+}
+
+   return 0;
+}
+
