1d10d288860eb40530bff205858398bacefc5f33
MdRayhan <md.a.a.rayhan@gmail.com>
2026-08-14 19:59:19 +0600

    Create Count Letters.cpp

---
+++ Count Letters.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+string s;
+cin>>s;
+int count[26]={0};
+for(int i=0;i<s.size();i++){
+    count[s[i]-'a']++;
+}
+for(int i=0;i<26;i++){
+    if(count[i]>0){
+    cout<<char('a'+i)<<" : "<<count[i]<<endl;
+    }
+}
+ return 0;
+ }
